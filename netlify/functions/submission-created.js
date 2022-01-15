49b28573d6f3463f0b354fbd468000b99d865883
steve lee <stevelee@w3.org>
2022-01-15 19:00:09 +0000

    Update form function

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,8 +1,26 @@
 exports.handler = async function(event, context) {
 
     const body = event.body
+    const url = 'https://api.github.com/repos/wai/wai-course-list/dispatches';
+    const githubPAT = "GITHUB_PAT"; // keep this secret
 
-    console.log(`form submittedt:\n ${JSON.stringify(body)}`);
+
+    const https = require("https")
+/*
+    https.get("https://jsonplaceholder.typicode.com/todos/1", function(response) {
+    response.setEncoding("utf8")
+    response.on("data", console.log)
+    response.on("error", console.error)
+    }).on("error", console.error)
+
+      json: {
+        event_type: 'publish_blog',
+      },
+      headers: {
+        Authorization: "token " + githubPAT,
+      },
+*/
+      console.log(`form submitted:\n ${JSON.stringify(body)}`);
 
     return {
         statusCode: 200,
