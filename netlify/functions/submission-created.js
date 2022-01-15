b66413c77b6abcad1161a2a8a32a09f1d414999f
steve lee <stevelee@w3.org>
2022-01-15 19:01:51 +0000

    Update form function

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -4,9 +4,8 @@ exports.handler = async function(event, context) {
     const url = 'https://api.github.com/repos/wai/wai-course-list/dispatches';
     const githubPAT = "GITHUB_PAT"; // keep this secret
 
-
-    const https = require("https")
 /*
+    const https = require("https")
     https.get("https://jsonplaceholder.typicode.com/todos/1", function(response) {
     response.setEncoding("utf8")
     response.on("data", console.log)
