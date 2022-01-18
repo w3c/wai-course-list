aeafb07c3d7396d6c4c6cb8b8ef71946427892d1
steve lee <stevelee@w3.org>
2022-01-18 16:29:37 +0000

    Add logging

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -2,37 +2,51 @@ const https = require('https')
 
 exports.handler = async function(event, context) {
 
-    const formData = event.body.data)
-    
+    var payload
+    try { 
+        payload = JSON.parse(event.body)
+    } catch(e) { 
+        console.error(`Invalid JSON payload: ${event.body}`)
+        return {
+            statusCode: 500,
+            body: 'Invalid JSON payload'
+        };
+    }
+
+    const formData = payload.data
+
     const body= `{
         "event_type": "netlify-form-submission",
         "client_payload": 
             ${JSON.stringify(formData)}`
-        console.log(body)
 
-        const options = {
-            hostname: 'api.github.com',
-            port: 443,
-            path: '/repos/w3c/wai-course-list/dispatches',
-            method: 'POST',
-            headers: {
-                'Accept': 'application/vnd.github.v3+json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.GITHUB_PAT}`,
-                'Content-Length': body.length
-            }        
-        }
-    
+    const options = {
+        hostname: 'api.github.com',
+        port: 443,
+        path: '/repos/w3c/wai-course-list/dispatches',
+        method: 'POST',
+        headers: {
+            'Accept': 'application/vnd.github.v3+json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.GITHUB_PAT}`,
+            'Content-Length': body.length
+        }        
+    }
+
+    // TODO maybe promisify so we can return the final outcome(s)
     const req = https.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-        
+        console.log(`Github statusCode: ${res.statusCode}`)
         res.on('data', d => {
-            console.info(d)
+            console.info(d.toString())
         })
     })
         
     req.on('error', error => {
-        console.log(error)
+        console.error(error)
+        return {
+            statusCode: 500,
+            body: `Error calling GitHub action - ${error}`
+        };
     })    
   
     req.write(body)
